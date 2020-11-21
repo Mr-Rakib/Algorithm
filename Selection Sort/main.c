96d65fb167ab3f3b54b016ddd016fb9248121243
Mr-Rakib <mrhasan.334@gmail.com>
2020-11-21 12:52:23 +0600

    add selection sort

---
+++ Selection Sort/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Selection Sort
+// Worst Case: O(n^2) || Best case O(n^2)
+// Not Stable
+// Not Adaptive
+
+void SelectionSort(int *arr, int n ){
+    int min;
+    for(int i = 0; i<n ; i++){
+        min = i;
+        for(int j = i+1; j<n; j++){
+            if(arr[min]> arr[j]){
+                min = j;
+            }
+        }
+        //swap(a[min, a[i]);
+        int temp = arr[min];
+        arr[min] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+void PrintAll(int *arr, int n)
+{
+     for(int i = 0; i<n ; i++)
+     {
+         printf("%d ", arr[i]);
+     }
+}
+int main()
+{
+    int arr[] = {11,55,1,6,3,9};
+    SelectionSort(arr, 6);
+    PrintAll(arr, 6);
+    return 0;
+}
